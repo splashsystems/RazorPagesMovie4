1578c3e47abdfe3acf163b73184709c892e3ee82
Scot <bscot.hagen@gmail.com>
2021-06-10 13:04:49 -0700

    Added Seed data

--- RazorPagesMovie4/Models/Movie.cs
+++ RazorPagesMovie4/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +11,10 @@ namespace RazorPagesMovie4.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
